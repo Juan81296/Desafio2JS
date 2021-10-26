21575b8283032cfcfd71e3dd6f8970bcca7290cd
Juan Tenaglia <juantenaglia81296@gmail.com>
2021-10-26 14:04:05 -0300

    Desafio2

---
+++ desafio2.js
@@ -0,0 +1,15 @@
+let usuario = prompt("Ingrese su usuario");
+if (usuario == "juan"){
+    alert("Bienvenido " + usuario);
+}
+else{
+    alert("Nombre de usuario incorrecto.")
+}
+
+let edad = prompt("Ingrese su edad");
+if (edad >= 18){
+    alert("Edad permitida.")
+}
+else{
+    alert("No es posible acceder si usted no es mayor de 18 años.")
+}
